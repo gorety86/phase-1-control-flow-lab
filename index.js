6e90df03fdd12849cbe3b2df74abbcc283e7ccb2
gorety <wagutugorety@gmail.com>
2023-03-21 11:24:04 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+if (ride <= 400){
+    return 'This one is on me!';
+    }if (ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.';
+    }else if(ride > 2500){
+        return 'No can do.';
+    }
+    }
+
+    function ternaryCheckCity(city) {
+    let response = city === "NYC"? 'Ok, sounds good.': 'No go.';
+    return response;
+    }
+    
+function switchOnCharmFromTip(Tip){
+  switch(Tip){
+    case "generous":
+        return "Thank you so much.";
+    case "not as generous":
+        return "Thank you.";
+    default: 
+        return "Bye.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+    
+        
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
